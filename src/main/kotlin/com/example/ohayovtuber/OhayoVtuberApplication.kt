f12ec4d20ceddc1df81a73da6309a86004023fc7
umi <0123mark3210@gmail.com>
2022-04-07 00:43:26 +0900

    first commit

---
+++ src/main/kotlin/com/example/ohayovtuber/OhayoVtuberApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ohayovtuber
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OhayoVtuberApplication
+
+fun main(args: Array<String>) {
+	runApplication<OhayoVtuberApplication>(*args)
+}
