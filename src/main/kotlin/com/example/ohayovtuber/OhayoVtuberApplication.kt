8f04172e575af0d6388d7cbd4e59c7b018459b71
umi <0123mark3210@gmail.com>
2022-04-07 21:20:22 +0900

    controllerのテストを追加

--- src/main/kotlin/com/example/ohayovtuber/OhayoVtuberApplication.kt
+++ src/main/kotlin/com/example/ohayovtuber/OhayoVtuberApplication.kt
@@ -1,7 +1,10 @@
 package com.example.ohayovtuber
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class OhayoVtuberApplication
