24fc1b9ea21e0c7d2e03fc3bae89e2aea4ffffba
umi <0123mark3210@gmail.com>
2022-04-19 00:53:46 +0900

    リポジトリ saveのユニットテストを追加

--- src/main/kotlin/com/example/oshikara/scenarioeditor/domain/talent/TalentRepository.kt
+++ src/main/kotlin/com/example/oshikara/scenarioeditor/domain/talent/TalentRepository.kt
@@ -2,4 +2,6 @@ package com.example.oshikara.scenarioeditor.domain.talent
 
 interface TalentRepository {
     fun insert(talent: Talent)
+
+    fun update(talent: Talent)
 }
--- src/test/kotlin/com/example/oshikara/scenarioeditor/infrastructure/impl/domain/talent/TalentRepositoryImplTest.kt
+++ src/test/kotlin/com/example/oshikara/scenarioeditor/infrastructure/impl/domain/talent/TalentRepositoryImplTest.kt
@@ -1,10 +1,17 @@
 package com.example.oshikara.scenarioeditor.infrastructure.impl.domain.talent
 
 import com.example.oshikara.scenarioeditor.domain.talent.Talent
+import com.example.oshikara.scenarioeditor.domain.talent.TalentId
 import com.example.oshikara.scenarioeditor.domain.talent.TalentName
 import com.example.oshikara.scenarioeditor.domain.talent.TalentRepository
+import com.example.oshikara.scenarioeditor.domain.talent.TalentStatus
 import com.example.oshikara.scenarioeditor.infrastructure.impl.domain.TestUtil
+import com.example.oshikara.scenarioeditor.infrastructure.model.Talents
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -29,21 +36,43 @@ internal class TalentRepositoryImplTest(
 
     @Test
     fun `タレントの作成に成功する`() {
+        // FIXME DBの検証に書き直す
         val talent = Talent.create(
             name = TalentName("夢見 太郎")
         )
         talentRepository.insert(talent)
     }
 
-//    @Test
-//    fun `タレントの名前の変更に成功する`() {
-//        val talent = Talent.create(
-//            name = TalentName("夢見 太郎")
-//        )
-//        talentRepository.insert(talent)
-//
-//        talent.changeName("変換 誤太郎")
-//        val changeNameTalent = talentRepository.update(talent)
-//
-//    }
+    @Test
+    fun `insertしたタレントでtalentIdが同じ時updateで更新できる`() {
+        val talentId = TalentId("b3816239-389c-4dc1-a8e8-ccc63d3bc011")
+        val talentName = TalentName("tanaka taro")
+        val talentStatus = TalentStatus.PRIVATE
+        transaction {
+            Talents.insert {
+                it[id] = talentId.value
+                it[name] = talentName.value
+                it[status] = talentStatus.name
+            }
+        }
+        val newTalentName = TalentName("yamada hanako")
+        val newTalentStatus = TalentStatus.PUBLIC
+        val newTalent = Talent.reconstruct(
+            id = talentId,
+            name = newTalentName,
+            status = newTalentStatus
+        )
+
+        talentRepository.update(newTalent)
+
+        val updateTalent = transaction {
+            Talents.select { Talents.id eq talentId.value }.single()
+        }
+
+        assertEquals(updateTalent[Talents.id], talentId.value)
+        assertEquals(updateTalent[Talents.name], newTalentName.value)
+        assertEquals(updateTalent[Talents.status], newTalentStatus.name)
+    }
+
+    // TODO : talentIdが異なるときDBが更新されないことを証明するユニットテストを追加する
 }
