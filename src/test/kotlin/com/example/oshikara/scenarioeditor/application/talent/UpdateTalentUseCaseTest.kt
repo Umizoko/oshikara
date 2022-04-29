58a4fd08d498ca85502033c9d608fafec8cb9f3a
Maruyama Kai <0123mark3210@gmail.com>
2022-04-29 20:16:56 +0900

    タレントステータス更新の実装

--- src/test/kotlin/com/example/oshikara/scenarioeditor/application/talent/UpdateTalentUseCaseTest.kt
+++ src/test/kotlin/com/example/oshikara/scenarioeditor/application/talent/UpdateTalentUseCaseTest.kt
@@ -4,6 +4,7 @@ import com.example.oshikara.scenarioeditor.domain.talent.Talent
 import com.example.oshikara.scenarioeditor.domain.talent.TalentId
 import com.example.oshikara.scenarioeditor.domain.talent.TalentName
 import com.example.oshikara.scenarioeditor.domain.talent.TalentRepository
+import com.example.oshikara.scenarioeditor.domain.talent.TalentStatus
 import com.example.oshikara.scenarioeditor.infrastructure.factory.TestTalentFactory
 import io.mockk.CapturingSlot
 import io.mockk.Runs
@@ -30,9 +31,10 @@ internal class UpdateTalentUseCaseTest {
         every { talentRepository.findById(talent.id) } returns talent
         every { talentRepository.update(capture(talentCapturingSlog)) } just Runs
 
-        val command = UpdateTalentUseCaseCommand(
-            talentId = TalentId(talent.id.value),
-            talentName = TalentName("name after update")
+        val command = UpdateTalentUseCaseDpo(
+            talentId = talent.id,
+            talentName = TalentName("name after update"),
+            talentStatus = TalentStatus.PRIVATE
         )
         updateTalentUseCase.execute(command)
 
@@ -47,14 +49,55 @@ internal class UpdateTalentUseCaseTest {
         val talent = TestTalentFactory.create()
         every { talentRepository.findById(talent.id) } returns null
 
-        val command = UpdateTalentUseCaseCommand(
+        val command = UpdateTalentUseCaseDpo(
             talentId = talent.id,
-            talentName = TalentName("name after update")
+            talentName = TalentName("name after update"),
+            talentStatus = TalentStatus.PRIVATE
         )
 
         val exception = assertThrows<UpdateTalentUseCaseException.NotFoundTalentException> {
             updateTalentUseCase.execute(command)
         }
         assertEquals(exception.message, "Not found talent resource. talentId: ${talent.id.value}")
+    }
+
+    @Test
+    fun `タレントIDを渡して公開にすると、ステートの値が公開に更新されたタレントが保存される`() {
+        val talent = TestTalentFactory.create()
+        val talentCapturingSlog: CapturingSlot<Talent> = slot()
+        every { talentRepository.findById(talent.id) } returns talent
+        every { talentRepository.update(capture(talentCapturingSlog)) } just Runs
+
+        val command = UpdateTalentUseCaseDpo(
+            talentId = TalentId(talent.id.value),
+            talentName = TalentName("name after update"),
+            talentStatus = TalentStatus.PUBLIC
+        )
+        updateTalentUseCase.execute(command)
+
+        val capturedTalent: Talent = talentCapturingSlog.captured
+
+        assertEquals(command.talentId, capturedTalent.id)
+        assertEquals(command.talentStatus, capturedTalent.status)
+    }
+
+    @Test
+    fun `タレントIDを渡して非公開にすると、ステートの値が非公開に更新されたタレントが保存される`() {
+        val talent = TestTalentFactory.create()
+        val talentCapturingSlog: CapturingSlot<Talent> = slot()
+        every { talentRepository.findById(talent.id) } returns talent
+        every { talentRepository.update(capture(talentCapturingSlog)) } just Runs
+
+        val command = UpdateTalentUseCaseDpo(
+            talentId = TalentId(talent.id.value),
+            talentName = TalentName("name after update"),
+            talentStatus = TalentStatus.PRIVATE
+        )
+        updateTalentUseCase.execute(command)
+
+        val capturedTalent: Talent = talentCapturingSlog.captured
+
+        assertEquals(command.talentId, capturedTalent.id)
+        assertEquals(command.talentStatus, capturedTalent.status)
     }
 }
