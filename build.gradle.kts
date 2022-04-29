887997ae6b0bbd94a431aeb5210d19a5a088a474
Maruyama Kai <kai.maruyama@claves.co.jp>
2022-04-29 11:29:15 +0900

    kotlintのプラグインを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("io.gitlab.arturbosch.detekt") version "1.20.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     kotlin("jvm") version "1.6.20"
     kotlin("plugin.spring") version "1.6.20"
 }
@@ -17,7 +18,6 @@ repositories {
 }
 
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
     runtimeOnly("mysql:mysql-connector-java:8.0.28")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.7")
     implementation("org.springframework.boot:spring-boot-starter-web:2.6.7")
@@ -42,8 +42,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-detekt {
-    autoCorrect = true
 }
