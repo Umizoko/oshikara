be3a5dcaa9b0224ba3b364485a8d1a9621c6a011
Maruyama Kai <0123mark3210@gmail.com>
2022-05-22 23:57:22 +0900

    EBへ自動デプロイするワークフローの設定

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,25 +11,23 @@ plugins {
 }
 
 group = "com.example"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_16
+version = "0.0.2-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    runtimeOnly("mysql:mysql-connector-java:8.0.28")
     implementation("com.h2database:h2:2.1.212")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.7")
     implementation("org.springframework.boot:spring-boot-starter-web:2.6.7")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.7")
-    testImplementation("io.mockk:mockk:1.12.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+    testImplementation("io.mockk:mockk:1.12.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.flywaydb:flyway-core:8.5.9")
-    implementation("org.flywaydb:flyway-mysql:8.5.9")
+    implementation("org.flywaydb:flyway-core:8.5.10")
     implementation("org.jetbrains.exposed:exposed-core:0.38.2")
     implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
@@ -38,7 +36,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "16"
+        jvmTarget = "11"
     }
 }
 
